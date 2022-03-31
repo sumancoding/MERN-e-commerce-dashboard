cc527a9118407ce54fb3e5799a5daba747b6c24e
suman <sumall@utu.fi>
2022-03-31 21:41:32 +0300

    schemas and model

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("App is working");
-});
+const connectDB = async () => {
+  mongooose.connect("mongodb://localhost:27017");
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model("product", productSchema);
+  const data = await product.find();
+  console.log(data);
+};
+connectDB();
 
 app.listen(5000);
