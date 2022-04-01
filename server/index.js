158b0bf5092246e9550e4243a63989324a43b4b1
suman <sumall@utu.fi>
2022-04-01 20:59:20 +0300

    nongo atlas connection

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
-const connectDB = async () => {
-  mongooose.connect("mongodb://localhost:27017");
-  const productSchema = new mongoose.Schema({});
-  const product = mongoose.model("product", productSchema);
-  const data = await product.find();
-  console.log(data);
-};
-connectDB();
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb+srv://tsuman49:XrcKBhufqKZ1ISL8@cluster0.ghjf7.mongodb.net/tutorial?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("db connection done");
+  });
