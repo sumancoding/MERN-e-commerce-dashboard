0c09e0c4a57882c59d21cbf54941670fab016e3f
suman <sumall@utu.fi>
2022-04-07 02:12:11 +0300

    login api in node

--- server/index.js
+++ server/index.js
@@ -10,7 +10,24 @@ app.use(cors()); //write it as a function
 app.post("/signup", async (req, res) => {
   let data = new userModel(req.body);
   let result = await data.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  if (req.body.email && req.body.password) {
+    let user = await userModel.findOne(req.body).select("-password"); //this removes password
+
+    if (user) {
+      res.send(user);
+    } else {
+      res.send("No result found");
+    }
+  } else {
+    console.log("Not found !!");
+  }
 });
 
 app.listen(5000);
